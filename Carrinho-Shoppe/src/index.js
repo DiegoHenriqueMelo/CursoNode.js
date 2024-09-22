9dd0fb86f799963e1feac69a2ef22f6781c69756
Diego Melo <140443998+DiegoHenriqueMelo@users.noreply.github.com>
2024-09-22 15:24:03 -0300

    Finalizando o Desafio do Carrinho de Compras

---
+++ Carrinho-Shoppe/src/index.js
@@ -0,0 +1,20 @@
+import createItem from "./services/iten.js";
+import * as cartService from "./services/cart.js";
+
+const myCart = [];
+
+const myWhishList = [];
+
+console.log("Welcome to the your Shoppe Cart");
+
+const iten1 = await createItem("Mouse Gamer", 9, 1);
+const iten2 = await createItem("Mikey Mouse", 12, 5);
+
+await cartService.addIten(myCart, iten1);
+await cartService.addIten(myCart, iten2);
+
+await cartService.displayCart(myCart);
+
+await cartService.removeItem(myCart, 1)
+
+await cartService.calculateTotal(myCart);
---
+++ Carrinho-Shoppe/src/services/cart.js
@@ -0,0 +1,38 @@
+async function addIten(userCart, iten) {
+  userCart.push(iten);
+}
+
+async function deleteIten(userCart, name) {
+  const index = userCart.findIndex((item) => item.name === name);
+
+  if (index !== -1) {
+    userCart.splice(index, 1);
+  }
+}
+
+async function removeItem(userCart, index) {
+  if (index >= 0 && index < userCart.length) {
+    userCart.splice(index, 1);
+  }
+  console.log(`\n❌ Você removeu o iten ${index} do Carrinho`)
+  
+}
+
+async function calculateTotal(userCart) {
+  const result = userCart.reduce((total, iten) => total + iten.subtotal(), 0);
+
+  console.log(`\n🎁 Total da Compra: ${result}`)
+}
+
+async function displayCart(userCart) {
+  console.log("\nShopee Cart List:\n");
+  userCart.forEach((iten, index) => {
+    console.log(
+      `${index + 1}. ${iten.name} - R$${iten.price} | ${
+        iten.quantity
+      }x | Subtotal: R$${iten.subtotal()} `
+    );
+  });
+}
+
+export { addIten, calculateTotal, deleteIten, removeItem, displayCart };
